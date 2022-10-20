feaf486236c15750fc8d4d051e3da57bd48bcadd
Zac Siegel <84301367+ZacSiegel@users.noreply.github.com>
2022-10-20 13:30:05 -0700

    Create roman_to_integer.js

---
+++ Strings/roman_to_integer.js
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/roman-to-integer/
+
+const romanToInt = (s) => {
+    
+const romans = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000
+    }
+    let total = 0
+    
+    for(let i = 0; i< s.length; i++){
+        if(romans[s[i]] < romans[s[i+1]]){
+            total -= romans[s[i]]
+        } else {
+            total += romans[s[i]]
+        }
+    }
+    return total
+};

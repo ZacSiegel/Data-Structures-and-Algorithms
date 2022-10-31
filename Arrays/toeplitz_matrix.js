17afbd8f087d4ca0fdab3e6dc938a03d599a5e52
Zac Siegel <84301367+ZacSiegel@users.noreply.github.com>
2022-10-31 12:06:33 -0700

    Create toeplitz_matrix.js

---
+++ Arrays/toeplitz_matrix.js
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/toeplitz-matrix/
+
+const isToeplitzMatrix = (matrix) => {
+    for(let i = 0; i <matrix.length-1; i++){
+        for(let j = 0; j < matrix[i].length-1; j++){
+            if(matrix[i][j] !== matrix[i+1][j+1]) return false
+        }
+    }
+    return true
+};

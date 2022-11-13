7be7276d40fe9a5bb501a3b100bad734f7de96fb
Zac Siegel <84301367+ZacSiegel@users.noreply.github.com>
2022-11-12 19:01:20 -0800

    Update insert_interval.js

--- Intervals/insert_interval.js
+++ Intervals/insert_interval.js
@@ -6,7 +6,7 @@ const insert = (intervals, newInterval) => {
 	for(let i = 0; i < intervals.length; i++){
 
 		// if new interval end is before the current element without overlapping
-    // we simply add it to the beginning of the intervals list
+    		// we simply add it to the beginning of the intervals list
 		if(newInterval[1] < intervals[i][0]){
 			res.push(newInterval)
             // merge [newInterval] and [intervals]
@@ -15,7 +15,7 @@ const insert = (intervals, newInterval) => {
 		}
 
 		// if new interval is after the current element without overlapping
-    // res = [] =>    res = [[intervals[i],...]
+    		// res = [] =>    res = [[intervals[i],...]
 		else if(newInterval[0] > intervals[i][1]){
 			res.push(intervals[i])
 		}
@@ -26,9 +26,9 @@ const insert = (intervals, newInterval) => {
 		}
 
 	}
-	//If we are out of the loop then it means that we have scanned through all the intervals and 
-	//have our newInterval updated/merged but we haven't placed it into the original intervals list
-  // this means that the end of the newly merged interval is larger than any interval in the original intervals list
+	// if we are out of the loop then it means that we have scanned through all the intervals and 
+	// have our newInterval updated/merged but we haven't placed it into the original intervals list
+  	// this means that the end of the newly merged interval is larger than any interval in the original intervals list
 
 	res.push(newInterval)
 	return res

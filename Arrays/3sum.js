d97a28d091fcdf2eb95c14f7c0985afbbdb1a1f1
Zac Siegel <84301367+ZacSiegel@users.noreply.github.com>
2022-11-13 14:30:40 -0800

    Update 3sum.js

--- Arrays/3sum.js
+++ Arrays/3sum.js
@@ -1,31 +1,35 @@
 // https://leetcode.com/problems/3sum/
 
 const threeSum = (nums) => {
-    nums = nums.sort((a,b)=> a- b)
-    let result = []
-    for(let i = 0; i < nums.length - 2; i++){
+    nums = nums.sort((a, b) => a - b);
+
+    let result = [];
+    for (let i = 0; i < nums.length - 2; i++) {
         // making sure not to use same start value twice
-        if( i > 0 && nums[i] === nums[i-1]) continue
-        let left = i + 1
-        let right = nums.length-1
-        while(left < right){
-            let threeSum = nums[i] + nums[left] + nums[right]
-            if(threeSum === 0){
-                // add nums to result 
-                result.push([nums[i], nums[left], nums[right]])
-                // to prevent duplicates, we use a while loop to make sure left
-                // and right pointers don't land on the same num again
-                while(nums[left] === nums[left + 1]) left++
-                while(nums[right] === nums[right - 1]) right--
-                // once we know the nums are not dupes, we move both pointers
-                left++
-                right--
-            } else if (threeSum < 0){
-                left++
+        if (i > 0 && nums[i] === nums[i - 1]) continue;
+
+        let left = i + 1;
+        let right = nums.length - 1;
+        
+        while (left < right) {
+            let threeSum = nums[i] + nums[left] + nums[right];
+            if (threeSum < 0) {
+                left++;
+            } else if (threeSum > 0) {
+                right--;
             } else {
-                right--
+                // add nums to result
+                result.push([nums[i], nums[left], nums[right]]);
+                // move left pointer up
+                left++;
+                // if left pointer lands on dupe, move it up until it doesn't
+                while (nums[left] === nums[left - 1] && left < right) {
+                    left++;
+                }
             }
+
         }
+
     }
     return result
 };

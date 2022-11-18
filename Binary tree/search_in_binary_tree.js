3135a4372ddd0bf2f08364958d230e5b13a26b35
Zac Siegel <84301367+ZacSiegel@users.noreply.github.com>
2022-11-17 19:32:34 -0800

    Create search_in_binary_tree.js

---
+++ Binary tree/search_in_binary_tree.js
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/search-in-a-binary-search-tree/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+
+const searchBST = (root, val) => {
+    while(root !== null){
+        if(val < root.val){
+            root = root.left;
+        } else if (val > root.val){
+            root = root.right;
+        } else {
+            return root
+        }
+    }
+    return null
+};

ed8b0d892de320c499fd104165988ea655ab6588
Zac Siegel <84301367+ZacSiegel@users.noreply.github.com>
2022-12-05 12:33:10 -0800

    Create invert_binary_tree_refactored.js

---
+++ Binary Tree/invert_binary_tree_refactored.js
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/invert-binary-tree/
+
+const invertTree = (root) => {
+    if(!root) return null;
+    
+    let temp = root.left;
+    root.left = root.right;
+    root.right = temp;
+    
+    invertTree(root.left);
+    invertTree(root.right);
+    
+    return root;
+};

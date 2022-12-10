094bd2ef30420d2273643ab7ec9983a032d26d53
Zac Siegel <84301367+ZacSiegel@users.noreply.github.com>
2022-12-09 21:48:40 -0800

    Update largest_positive_integer_with_negative.js

--- Arrays/largest_positive_integer_with_negative.js
+++ Arrays/largest_positive_integer_with_negative.js
@@ -4,12 +4,12 @@ const findMaxK = (nums) => {
     const map = new Map();
     let maxResult = 0;
     for(let i = 0; i < nums.length; i++){
-        let negative = nums[i] * -1   
+        let negative = nums[i] * -1;   
         if(map.has(negative)){
-            let currMax = Math.max(nums[i], map.get(negative))
-            maxResult = Math.max(maxResult, currMax)
-        }
-        map.set(nums[i], nums[i])
-    }
+            let currMax = Math.max(nums[i], map.get(negative));
+            maxResult = Math.max(maxResult, currMax);
+        };
+        map.set(nums[i], nums[i]);
+    };
     return maxResult === 0 ? -1 : maxResult
 };

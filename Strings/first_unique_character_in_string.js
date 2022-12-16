9611791f775d38450c28f8e597580b7ce7786f04
Zac Siegel <84301367+ZacSiegel@users.noreply.github.com>
2022-12-16 12:48:15 -0800

    Update first_unique_character_in_string.js

--- Strings/first_unique_character_in_string.js
+++ Strings/first_unique_character_in_string.js
@@ -1,9 +1,9 @@
 // https://leetcode.com/problems/first-unique-character-in-a-string/
 
 const firstUniqChar = (s) => {
-    let map = {}
+    let map = {};
     for(let char of s){
-        map[char] ? map[char]++ : map[char] = 1
+        map[char] ? map[char]++ : map[char] = 1;
     }
     for(let i = 0; i < s.length; i++){
        if(map[s[i]] === 1) return i

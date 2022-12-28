b6a90e5b1bf1450df0f7bb1a0582f526ffd753f5
Zac Siegel <84301367+ZacSiegel@users.noreply.github.com>
2022-12-27 21:24:03 -0800

    Update running_sum.ts

--- Arrays/running_sum.ts
+++ Arrays/running_sum.ts
@@ -1,7 +1,7 @@
 function runningSum(nums: number[]): number[] {
-    let sum: number[] = [nums[0]]
+    let sum: number[] = [nums[0]];
     for(let i = 1; i < nums.length; i++){
         sum.push(nums[i] + sum[i-1])
     }
-    return sum
+    return sum;
 };

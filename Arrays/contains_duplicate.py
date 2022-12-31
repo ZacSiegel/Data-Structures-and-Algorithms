02bbe80c14baf28dceac4f4ebcc3be04da1051a4
Zac Siegel <84301367+ZacSiegel@users.noreply.github.com>
2022-12-30 22:08:36 -0800

    Delete contains_duplicate.py

--- Arrays/contains_duplicate.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        seen = {}
-        for num in nums:
-            if num in seen:
-                return True
-            else:
-                seen[num] = 1
-        return False
